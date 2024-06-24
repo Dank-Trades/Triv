6c93d4d0fcfd4ef786777f074f2b735c2979c09a
its_rey <reydangelo219@gmail.com>
2024-06-24 23:47:18 +0630

    just admin commands

---
+++ cogs/admin_utils.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import os
+import subprocess
+from discord.app_commands import Group, command
+
+class admin_utils(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    def cog_check(self, ctx):
+        return ctx.author.id == self.client.owner_id
+    
+    @commands.command()
+    @commands.is_owner()  # Restrict command to bot owner
+    async def exec(ctx, *, command):
+        """Execute a terminal command."""
+        try:
+            # Run the command and capture the output
+            result = subprocess.run(command, shell=True, capture_output=True, text=True)
+            output = result.stdout if result.stdout else result.stderr
+            
+            # Limit output length to prevent excessive message length
+            if len(output) > 2000:
+                output = output[:1997] + '...'
+            
+            await ctx.send(f'```{output}```')
+        except Exception as e:
+            await ctx.send(f'Error: {e}')
+
+
+
+async def setup(client):
+    await client.add_cog(admin_utils(client))
